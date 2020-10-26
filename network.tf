d5ced41efafc508622b6870a53c1549a64043920
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-26 17:20:26 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -4,6 +4,12 @@ resource "ibm_is_vpc" "iac_iks_vpc_1" {
   address_prefix_management = "manual"
 }
 
+resource "ibm_is_vpc" "iac_iks_vpc_2" {
+  name = "${var.project_name}-${var.environment}-vpc"
+  resource_group = data.ibm_resource_group.group.id
+  address_prefix_management = "manual"
+}
+
 resource "ibm_is_vpc_address_prefix" "vpc_address_prefix" {
   count                     = local.max_size
   name                      = "${var.project_name}-${var.environment}-range-${format("%02s", count.index)}"
@@ -25,39 +31,4 @@ resource "ibm_is_subnet" "iac_iks_subnet" {
   depends_on  = [ibm_is_vpc_address_prefix.vpc_address_prefix]
 }
 
-resource "ibm_is_vpc" "iac_iks_vpc_2" {
-  name = "${var.project_name}-${var.environment}-vpc"
-  resource_group = data.ibm_resource_group.group.id
-  address_prefix_management = "manual"
-}
-
-resource "ibm_is_security_group_rule" "iac_iks_security_group_rule_tcp_k8s" {
-  count     = local.max_size
-  group     = ibm_is_vpc.iac_iks_vpc_1.default_security_group
-  direction = "inbound"
-  remote    = ibm_is_subnet.iac_iks_subnet[count.index].ipv4_cidr_block
-
-  tcp {
-    port_min = 30000
-    port_max = 32767
-  }
-
-   }
-resource "ibm_tg_gateway" "new_tg_gw"{
-  name      = "front-office-tg"
-  location  = "eu-de"
-  global    =  true
-  resource_group="6664a071c0b546deb4703269b54a5d9a"
-}
-
-resource "ibm_is_public_gateway" "iac_iks_gateway" {
-    name  = "${var.project_name}-${var.environment}-gateway-${format("%02s", count.index)}"
-    vpc   = ibm_is_vpc.iac_iks_vpc_1.id
-    zone  = var.vpc_zone_names[count.index]
-    count = local.max_size
-
-    //User can configure timeouts
-    timeouts {
-        create = "90m"
-    }
-}
+ }
