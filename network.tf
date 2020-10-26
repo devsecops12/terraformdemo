bdf26d140d3e3a9293de2b6712679a8fd2e47776
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-26 17:41:36 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,10 +1,10 @@
-resource "ibm_is_vpc" "iac_iks_vpc_1" {
+resource "ibm_is_vpc" "iac_iks_vpc" {
   name = "${var.project_name}-${var.environment}-vpc"
   resource_group = data.ibm_resource_group.group.id
   address_prefix_management = "manual"
 }
 
-resource "ibm_is_vpc" "iac_iks_vpc_2" {
+resource "ibm_is_vpc" "iac_iks_vpc" {
   name = "${var.project_name}-${var.environment}-vpc"
   resource_group = data.ibm_resource_group.group.id
   address_prefix_management = "manual"
@@ -14,7 +14,7 @@ resource "ibm_is_vpc_address_prefix" "vpc_address_prefix" {
   count                     = local.max_size
   name                      = "${var.project_name}-${var.environment}-range-${format("%02s", count.index)}"
   zone                      = var.vpc_zone_names[count.index]
-  vpc                       = ibm_is_vpc.iac_iks_vpc_1.id
+  vpc                       = ibm_is_vpc.iac_iks_vpc.id
   cidr                      = "172.26.${format("%01s", count.index)}.0/24"
 }
 
@@ -22,7 +22,7 @@ resource "ibm_is_subnet" "iac_iks_subnet" {
   count                    = local.max_size
   name                     = "${var.project_name}-${var.environment}-subnet-${format("%02s", count.index)}"
   zone                     = var.vpc_zone_names[count.index]
-  vpc                      = ibm_is_vpc.iac_iks_vpc_1.id
+  vpc                      = ibm_is_vpc.iac_iks_vpc.id
   ipv4_cidr_block          = "172.26.${format("%01s", count.index)}.0/26"
   # total_ipv4_address_count = 64
   
